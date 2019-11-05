1c9538156ec07305ecc1c74cc180a8a06a517e3e
Justin Russell <justinrussell@Justins-MacBook-Pro.local>
2019-11-04 18:40:42 -0800

    First componet added and working with toolbox

---
+++ App.js
@@ -0,0 +1,195 @@
+import React from "react";
+import _ from "lodash";
+import '../node_modules/react-grid-layout/css/styles.css';
+import '../node_modules/react-resizable/css/styles.css';
+import './App.css';
+import { Responsive, WidthProvider } from "react-grid-layout";
+//import ChatComponent from './components/ChatComponent.js';
+//import QuickCommands from './components/QuickCommands.js';
+//import QuickCommandsList from './components/QuickCommandsList.js';
+import ChatSettings from './components/chatSettings.js';
+//import SpeechCommands from './components/SpeechCommands.js';
+//import threedeespace from './components/threedeespace.js';
+//import io from 'socket.io-client';
+//const socket = io('https://fusionpaloalto.elliotsyoung.com');
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+class ToolBoxItem extends React.Component {
+  render() {
+    return (
+      <div
+        className="toolbox__items__item"
+        onClick={this.props.onTakeItem.bind(undefined, this.props.item)}
+      >
+        {this.props.item.i}
+      </div>
+    );
+  }
+}
+class ToolBox extends React.Component {
+  render() {
+    return (
+      <div className="toolbox">
+        <span className="toolbox__title"><center>Toolbox</center></span>
+        <div className="toolbox__items">
+          {this.props.items.map(item => (
+            <ToolBoxItem
+              key={item.i}
+              item={item}
+              onTakeItem={this.props.onTakeItem}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default class ToolboxLayout extends React.Component {
+  static defaultProps = {
+    className: "layout",
+    rowHeight: 30,
+    onLayoutChange: function() {},
+    cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+    initialLayout: generateLayout()
+  };
+
+  state = {
+    currentBreakpoint: "lg",
+    compactType: "vertical",
+    mounted: false,
+    layouts: { lg: this.props.initialLayout },
+    toolbox: { lg: [] }
+  };
+
+  componentDidMount() {
+    this.setState({ mounted: true });
+  }
+
+  generateDOM() {
+    return _.map(this.state.layouts[this.state.currentBreakpoint], l => {
+      return (
+        <div key={l.i} className={l.static ? "static" : ""}>
+          <div className="hide-button" onClick={this.onPutItem.bind(this, l)}>
+            &times;
+          </div>
+          {
+            <span className="text"><ChatSettings handleVoiceChange={this.handleVoiceChange}/></span>
+          }
+        </div>
+      );
+    });
+  }
+
+  onBreakpointChange = breakpoint => {
+    this.setState(prevState => ({
+      currentBreakpoint: breakpoint,
+      toolbox: {
+        ...prevState.toolbox,
+        [breakpoint]:
+          prevState.toolbox[breakpoint] ||
+          prevState.toolbox[prevState.currentBreakpoint] ||
+          []
+      }
+    }));
+  };
+
+  onCompactTypeChange = () => {
+    const { compactType: oldCompactType } = this.state;
+    const compactType =
+      oldCompactType === "horizontal"
+        ? "vertical"
+        : oldCompactType === "vertical"
+        ? null
+        : "horizontal";
+    this.setState({ compactType });
+  };
+
+  onTakeItem = item => {
+    this.setState(prevState => ({
+      toolbox: {
+        ...prevState.toolbox,
+        [prevState.currentBreakpoint]: prevState.toolbox[
+          prevState.currentBreakpoint
+        ].filter(({ i }) => i !== item.i)
+      },
+      layouts: {
+        ...prevState.layouts,
+        [prevState.currentBreakpoint]: [
+          ...prevState.layouts[prevState.currentBreakpoint],
+          item
+        ]
+      }
+    }));
+  };
+
+  onPutItem = item => {
+    this.setState(prevState => {
+      return {
+        toolbox: {
+          ...prevState.toolbox,
+          [prevState.currentBreakpoint]: [
+            ...(prevState.toolbox[prevState.currentBreakpoint] || []),
+            item
+          ]
+        },
+        layouts: {
+          ...prevState.layouts,
+          [prevState.currentBreakpoint]: prevState.layouts[
+            prevState.currentBreakpoint
+          ].filter(({ i }) => i !== item.i)
+        }
+      };
+    });
+  };
+
+  onLayoutChange = (layout, layouts) => {
+    this.props.onLayoutChange(layout, layouts);
+    this.setState({ layouts });
+  };
+
+  onNewLayout = () => {
+    this.setState({
+      layouts: { lg: generateLayout() }
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ToolBox
+          items={this.state.toolbox[this.state.currentBreakpoint] || []}
+          onTakeItem={this.onTakeItem}
+        />
+
+        <ResponsiveReactGridLayout
+          {...this.props}
+          layouts={this.state.layouts}
+          onBreakpointChange={this.onBreakpointChange}
+          onLayoutChange={this.onLayoutChange}
+          // WidthProvider option
+          measureBeforeMount={false}
+          // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
+          // and set `measureBeforeMount={true}`.
+          useCSSTransforms={this.state.mounted}
+          compactType={this.state.compactType}
+          preventCollision={!this.state.compactType}
+        >
+          {this.generateDOM()}
+        </ResponsiveReactGridLayout>
+      </div>
+    );
+  }
+}
+
+function generateLayout() {
+  return _.map(_.range(0, 1), function(item, i) {
+    return {
+      x: 0,
+      y: 0,
+      w: 4,
+      h: 2,
+      i: i.toString()
+        };
+  });
+}
